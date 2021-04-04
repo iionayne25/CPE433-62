80b951ed44256990ca54dfa1330ba3813570bb9d
iionayne25 <alinqweqwz@gmail.com>
2021-04-04 19:41:21 +0700

    hw3

--- Program.cs
+++ Program.cs
@@ -296,8 +296,7 @@ namespace DNWS
                     // Get one, show some info
                     _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
                     HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
-                    Thread thread1 = new Thread(new ThreadStart(hp.Process));
-                    thread1.Start();
+                    hp.Process();
                 }
                 catch (Exception ex)
                 {
